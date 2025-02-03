2bc9857ec1311b5495f961ae9c2f1aeb1c6bd84c
sanaulparvej <perves15-6433@s.diu.edu.bd>
2025-02-04 03:36:57 +0600

    update

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Assignment 2",style: TextStyle(fontSize: 25),),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: ,
+    );
+  }
+}
+
